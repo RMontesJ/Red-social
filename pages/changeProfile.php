57775e1fc4d5011495cc9e1588258d5a0f3c719f
[Rafael Montes] <rafaelmontesjimenez@gmail.com>
2024-07-28 23:10:51 +0200

    Valid change of profile info

--- pages/changeProfile.php
+++ pages/changeProfile.php
@@ -20,6 +20,7 @@ $extra_info = $DB->catchExtraInfo($id);
     <title>Edit profile</title>
     <link rel="stylesheet" href="../css/nav.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="../css/changeProfile.css?v=<?php echo time(); ?>">
+    <script src="../valid/changeProfile.js?v=<?php echo time(); ?>" defer></script>
 </head>
 <body>
 
@@ -30,13 +31,14 @@ $extra_info = $DB->catchExtraInfo($id);
 
 <div class="createPost">
 
-<form action="../logic/logic_changeProfile.php?id_user=<?php echo $id ?>" method="post" enctype="multipart/form-data">
+<form action="../logic/logic_changeProfile.php?id_user=<?php echo $id ?>" method="post" id="form" enctype="multipart/form-data">
 
 <h2>Actualizar perfil</h2>
 
 <div class="input-group">
 <label for="name">Nombre</label>
 <input type="text" name="name" id="name" value="<?php echo $name ?>" placeholder="Nombre nuevo">
+<p id="corregirName"></p>
 <label for="status">Estado</label>
 <input type="text" name="status" id="status" value="<?php echo $status ?>" placeholder="Estado nuevo">
 <label for="name">Informacion extra</label>
