ecbbdcf09dfb91f25e87e280ad75e3ed85ec2d30
[Rafael Montes] <rafaelmontesjimenez@gmail.com>
2024-07-30 23:00:06 +0200

    Puts default picture if its not set

--- logic/logic_post.php
+++ logic/logic_post.php
@@ -7,29 +7,33 @@ $DB = new DB();
 
 $title = $_POST['title'];
 $description = $_POST['description'];
-$fotoPredeterminada = '';
+$extension = $_FILES['picture']['type'];
 
 if(isset($_FILES['picture'])){
 
- // Ruta donde se guardará la foto (puedes ajustarla según tu estructura de archivos)
- $ruta_destino = '../posts-pictures/';
+    if(strpos($extension, 'png') || strpos($extension, 'jpeg') || strpos($extension, 'jpg') || strpos($extension, 'webp')){
 
- // Nombre del archivo original
- $nombre_archivo = $_FILES['picture']['name'];
+// Ruta donde se guardará la foto (puedes ajustarla según tu estructura de archivos)
+    $ruta_destino = '../posts-pictures/';
 
- // Mover el archivo desde el directorio temporal al directorio destino
- if (move_uploaded_file($_FILES['picture']['tmp_name'], $ruta_destino . $nombre_archivo)) {
-     // Aquí puedes guardar $nombre_archivo en la base de datos o realizar otras operaciones
-     $foto = $nombre_archivo;
- }
+// Nombre del archivo original
+    $nombre_archivo = $_FILES['picture']['name'];
+
+// Mover el archivo desde el directorio temporal al directorio destino
+if (move_uploaded_file($_FILES['picture']['tmp_name'], $ruta_destino . $nombre_archivo)) {
+    // Aquí puedes guardar $nombre_archivo en la base de datos o realizar otras operaciones
+    $foto = $nombre_archivo;
+    $DB->createPost($title, $description, $foto, $id);
+}
 
 }
 
-// requires title and description to post anything
-if(isset($title) && isset($description)){
-
-$DB->createPost($title, $description, $foto, $id);
+// si no has puesto foto, te pone la foto predeterminada
+else if ($_FILES['picture']['name'] == ""){
+    $foto = $fotoPredeterminada = '../posts-pictures/default-picture.png';
+    $DB->createPost($title, $description, $foto, $id);
 }
 
+}
 
 ?>
