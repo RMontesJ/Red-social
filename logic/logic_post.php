2d100bbe2b326f364f6627b089f0875b353b5c28
[Rafael Montes] <rafaelmontesjimenez@gmail.com>
2024-08-01 19:30:44 +0200

    Fixed upload of post with default picture

--- logic/logic_post.php
+++ logic/logic_post.php
@@ -4,7 +4,7 @@ $id = $_GET['id_user'];
 
 require_once "../DB/DB_Connection.php";
 $DB = new DB();
-$id_publisher = $DB->catchName($id);
+$name_publisher = $DB->catchName($id);
 
 $title = $_POST['title'];
 $description = $_POST['description'];
@@ -24,7 +24,7 @@ if(isset($_FILES['picture'])){
 if (move_uploaded_file($_FILES['picture']['tmp_name'], $ruta_destino . $nombre_archivo)) {
     // Aquí puedes guardar $nombre_archivo en la base de datos o realizar otras operaciones
     $foto = $nombre_archivo;
-    $DB->createPost($title, $description, $foto, $id, $id_publisher);
+    $DB->createPost($title, $description, $foto, $id, $name_publisher);
 }
 
 }
@@ -32,7 +32,7 @@ if (move_uploaded_file($_FILES['picture']['tmp_name'], $ruta_destino . $nombre_a
 // si no has puesto foto, te pone la foto predeterminada
 else if ($_FILES['picture']['name'] == ""){
     $foto = $fotoPredeterminada = '../posts-pictures/default-picture.png';
-    $DB->createPost($title, $description, $foto, $id);
+    $DB->createPost($title, $description, $foto, $id, $name_publisher);
 }
 
 else{
